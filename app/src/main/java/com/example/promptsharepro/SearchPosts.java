e86402a37d416fa042f5c94bc27f5f727dd04ad6
Darius Kashani <dkashani@usc.edu>
2024-11-05 16:31:32 -0800

    Save changes in SearchPosts.java before switching branches

--- app/src/main/java/com/example/promptsharepro/SearchPosts.java
+++ app/src/main/java/com/example/promptsharepro/SearchPosts.java
@@ -2,7 +2,7 @@ package com.example.promptsharepro;
 
 public class SearchPosts {
 
-    public void searchPosts(String query) {
+    public void searchPosts(String query) { // take
         //
     }
 }
